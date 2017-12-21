ef5d0df18caf8f3a5897ff3b72b27eed6d28c78e
slywind_lee <slywind_lee@slywind>
2017-12-21 15:12:34 +0900

    Add Algospot Project

---
+++ Algospot/src/fossil.cpp
@@ -0,0 +1,16 @@
+/*
+ * fossil.cpp
+ *
+ *  Created on: 2017. 12. 21.
+ *      Author: slywind_lee
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << "Hello Git Hub" << endl;
+
+	return 0;
+}
