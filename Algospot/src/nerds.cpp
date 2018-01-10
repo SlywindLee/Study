2ecd5b2e66192d406458981dfa896431e8f44507
SlywindLee <sly.wind.lee@gmail.com>
2018-01-10 17:53:16 +0900

    NERDS 문제 시작

---
+++ Algospot/src/nerds.cpp
@@ -0,0 +1,38 @@
+/* Algospot:Nerds */
+
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+/* Main */
+
+int main(){
+
+	vector< pair<int, int> > points[2], hull[2];
+	
+	// get inputs
+	
+	int n; cin >> n;
+	for(int i = 0; i < n; i++){
+		int a, b, c;
+		cin >> a >> b >> c;
+		points[a].push_back(make_pair(b, c));
+	}
+	
+	// make hull
+	
+	
+	
+	
+	
+	return 0;
+}
+
+/* Functions */
+
+
+
+
+
+
