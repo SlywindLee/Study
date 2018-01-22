4465c0cba12f56c17f45eb1300f27f6324d2af71
slywind <slywind@slynux>
2018-01-22 23:40:50 +0900

    전처리(블럭 돌리기) 완성

--- Algospot/src/boardcover2.cpp
+++ Algospot/src/boardcover2.cpp
@@ -7,12 +7,129 @@
 
 
 #include <iostream>
-#include <string>
+#include <vector>
+#include <stack>
+#include <utility>
+#define BOARDSIZE 10
+
 using namespace std;
+
+// global variables
+
+int H, W, R, C;
+char board[BOARDSIZE][BOARDSIZE], ablock[2][BOARDSIZE][BOARDSIZE];
+vector< vector< pair<int, int> > > blocks;
+
+void swap(int& a, int& b);
+void saveBlock(size_t n);
+void makePossibleBlock();
+
+// main
 
 int main(){
 
+	// get inputs
 
+	cin >> H >> W >> R >> C;
+
+	H--; W--; R--; C--;
+
+	for(int i = 0; i <= H; i++)
+		cin >> board[i];
+
+	for(int i = 0; i <= R; i++)
+		cin >> ablock[0][i];
+
+	// pre-calc
+
+	blocks.clear();
+
+	makePossibleBlock();
+
+	cout << blocks.size() << endl;
 
 	return 0;
 }
+
+// functions
+
+void swap(int& a, int& b){
+	int t = a; a = b; b = t;
+}
+
+void saveBlock(size_t n){
+
+	cout << n << endl;
+
+	vector< pair<int, int> > b;
+
+	// convert to coordinate using base point(left-upper)
+
+	int x = -1, y = -1;
+
+	for(int i = 0; i <= R; i++){
+		for(int j = 0; j <= C; j++){
+
+			cout << ablock[n][i][j];
+
+			if(ablock[n][i][j] == '#'){
+				if(x < 0){ x = i; y = j; b.push_back(make_pair(0, 0)); continue; }
+				b.push_back(make_pair(i - x, j - y));
+			}
+		}
+		cout << endl;
+	}
+	// push
+
+	blocks.push_back(b);
+
+	return;
+}
+
+void makePossibleBlock(){
+
+	// save
+
+	saveBlock(0);
+
+	// rotate & save
+
+	for(int n = 0; n < 3; n++){
+
+		// rotate
+
+		for(int i = 0; i <= R; i++)
+			for(int j = 0; j <= C; j++)
+				ablock[(n + 1) % 2][C - j][i] = ablock[n % 2][i][j];
+
+		swap(R, C);
+
+		// save
+
+		saveBlock((n + 1) % 2);
+	}
+
+	// eliminate dulplicates
+
+	stack<int> d;
+
+	for(int i = 1; i <= 3; i++)
+		for(int j = 0; j < i; j++){
+
+			bool same = true;
+
+			for(size_t k = 0; k < blocks[i].size(); k++)
+				if(blocks[i][k] != blocks[j][k]){ same = false; break;}
+
+			if(same){ d.push(i); break; }
+		}
+
+	while(!d.empty()){
+		blocks.erase(blocks.begin() + d.top());
+		d.pop();
+	}
+
+	return;
+}
+
+
